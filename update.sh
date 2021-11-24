4e0994586293c35e0699b64d4d66e9f3bd9e30fc
hooao <hooaosir@163.com>
2021-11-24 15:10:41 +0800

    Manually update cn ip Remove all_cn.txt Add freesurfing.us

--- update.sh
+++ update.sh
@@ -1,21 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+LATESTIPLIST="\
+https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest \
+https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest \
+https://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-latest \
+https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest "
 
 TMP_DIR=`mktemp -d /tmp/chinaip.XXXXXX`
-echo "Updating china ip"
-wget -q --no-check-certificate -O $TMP_DIR/ip http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest 
-cat $TMP_DIR/ip | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}'|cat >$TMP_DIR/all_cn.txt
+LISTCOUNT=0
+for list in $LATESTIPLIST; do
+	echo "downloading $list"
+    wget -q --no-check-certificate -O $TMP_DIR/ip.$LISTCOUNT $list
+	let LISTCOUNT++
+done
+echo "Updating CN and US"
 
+cat $TMP_DIR/ip.* | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}'|cat >$TMP_DIR/all_cn.txt
+cat $TMP_DIR/ip.* | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}'|cat >$TMP_DIR/all_us.txt
 sort -u $TMP_DIR/all_cn.txt | sed -r 's#(.+)#ip-cidr,\1, DIRECT#g' > freesurfing.ip
-cp $TMP_DIR/all_cn.txt ./
+sort -u $TMP_DIR/all_us.txt | sed -r 's#(.+)#ip-cidr,\1, PROXY#g' > freesurfing.us
+
 rm -rf $TMP_DIR
 
 echo "Updating gfwlist"
 ./gfwlist2dnsmasq.sh -o freesurfing
 
-diffcount=`git diff  freesurfing.ip freesurfing | wc -l`
+diffcount=`git diff  freesurfing.ip freesurfing freesurfing.us| wc -l`
 if [ $diffcount != 0 ]; then
    echo "Something changed."
-   git add freesurfing.ip freesurfing all_cn.txt
+   git add freesurfing.ip freesurfing freesurfing.us
    git commit -m "update`date '+%Y%m%d%H%M'`"
    git pull && git push
 else
