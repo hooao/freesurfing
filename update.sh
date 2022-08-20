bb405f7198f461e18648253fa1cde4fbc20e85dd
Hooao <hooaosir@163.com>
2022-08-20 03:13:29 +0000

    add freesurfing.chn

--- update.sh
+++ update.sh
@@ -24,21 +24,25 @@ cat $TMP_DIR/ip.* | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" int(32-log($5)/log(2)
 cat $TMP_DIR/ip.* | awk -F '|' '/US/&&/ipv4/ {print $4 "/" int(32-log($5)/log(2))}' | cat >$TMP_DIR/all_us.txt
 sort -u $TMP_DIR/all_cn.txt | sed -r 's#(.+)#ip-cidr,\1, DIRECT#g' >freesurfing.ip
 sort -u $TMP_DIR/all_cn.txt | sed -r 's#(.+)#IP-CIDR,\1, DIRECT#g' >freesurfing.surge
+echo "freesurfing.chn"
+sort -u $TMP_DIR/all_cn.txt | sed -r 's#(.+)#\1#g' >freesurfing.chn
+
 sort -u $TMP_DIR/all_us.txt | sed -r 's#(.+)#ip-cidr,\1, PROXY#g' >freesurfing.us
+
 
 rm -rf $TMP_DIR
 
 echo "Updating gfwlist"
-if [[ $1 == "--debug" ]]; then
-   echo "updating dnsmasq"
-else
-   ./gfwlist2dnsmasq.sh -o freesurfing
-fi
+# if [[ $1 == "--debug" ]]; then
+#    echo "updating dnsmasq"
+# else
+#    ./gfwlist2dnsmasq.sh -o freesurfing
+# fi
 
-diffcount=$(git diff freesurfing.ip freesurfing freesurfing.us freesurfing.surge | wc -l)
+diffcount=$(git diff freesurfing*  | wc -l)
 if [ $diffcount != 0 ]; then
    echo "Something changed."
-     git add freesurfing.ip freesurfing freesurfing.us freesurfing.surge
+     git add freesurfing*
      git commit -m "update`date '+%Y%m%d%H%M'`"
    if [[ $1 != "--nopush" && $1 != "--debug" ]]; then
       git pull && git push
