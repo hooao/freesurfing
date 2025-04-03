a2d714c9ec71000019abfc75752009d09c88d673
hooao <hooaosir@163.com>
2025-04-03 10:19:31 +0800

    script small fix

--- update.sh
+++ update.sh
@@ -25,12 +25,10 @@ awk -F/ '{ printf "DOMAIN-SUFFIX,%s, DIRECT\n",$2}' accelerated-domains.china.co
 echo "Updating CN and US"
 
 cat $TMP_DIR/ip.* | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" int(32-log($5)/log(2))}' | cat >$TMP_DIR/all_cn.txt
-#cat $TMP_DIR/ip.* | awk -F '|' '/US/&&/ipv4/ {print $4 "/" int(32-log($5)/log(2))}' | cat >$TMP_DIR/all_us.txt
 sort -uV $TMP_DIR/all_cn.txt | sed -r 's#(.+)#ip-cidr,\1, DIRECT#g' >freesurfing.chn.qx
 sort -uV $TMP_DIR/all_cn.txt | sed -r 's#(.+)#IP-CIDR,\1, DIRECT#g' >freesurfing.chn.surge
 echo "freesurfing.chn"
 sort -uV $TMP_DIR/all_cn.txt | sed -r 's#(.+)#\1#g' >freesurfing.chn
-sort -uV $TMP_DIR/all_us.txt | sed -r 's#(.+)#ip-cidr,\1, PROXY#g' >freesurfing.us
 
 echo "update asn"
 curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"   \
